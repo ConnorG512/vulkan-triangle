af62083b2711d6fa6a7750571a51204860168dc3
Connor Garey <connorgarey1999@gmail.com>
2025-07-29 13:54:03 +0100

    Added create info

--- src/hello-triangle-application.zig
+++ src/hello-triangle-application.zig
@@ -21,7 +21,7 @@ pub const HelloTriangleApplication = struct {
 
        const width: c_int = comptime 800;
        const height: c_int = comptime 600;
-       self.window = glfw.glfwCreateWindow(width, height, "Vulkan", null, null);
+       self.window = glfw.glfwCreateWindow(width, height, "Vulkan Triangle", null, null);
        const error_code: c_int = glfw.glfwGetError(null);
        if (error_code != 0) {
            std.log.err("GLFW ERROR: {d}\n", .{error_code});
@@ -48,6 +48,18 @@ pub const HelloTriangleApplication = struct {
             .pEngineName = "No Engine",
             .engineVersion = vk.VK_MAKE_VERSION(1, 0, 0),
             .apiVersion = vk.VK_API_VERSION_1_0,
+        };
+
+        const glfw_extension_count: c_int = 0;
+        const glfw_extentions: [*c]u8 = undefined;
+        glfw_extentions = glfw.glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+        const createInfo = vk.VkInstanceCreateInfo {
+            .sType = vk.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+            .pApplicationInfo = &appinfo,
+            .enabledExtensionCount = glfw_extension_count,
+            .ppEnabledExtensionNames = glfw_extentions,
+            .enabledLayerCount = 0,
         };
     }
 };
