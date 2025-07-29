15a040f3b3fd6ffe3b676c9a1ea0add4766da767
Connor Garey <connorgarey1999@gmail.com>
2025-07-29 13:41:04 +0100

    Added vkInstance struct.

--- src/hello-triangle-application.zig
+++ src/hello-triangle-application.zig
@@ -2,9 +2,11 @@ const std = @import("std");
 const assert = std.debug.assert;
 
 const glfw = @cImport(@cInclude("GLFW/glfw3.h"));
+const vk = @cImport(@cInclude("vulkan/vulkan.h"));
 
 pub const HelloTriangleApplication = struct {
     window: ?*glfw.GLFWwindow = null,
+    vkInstance: vk.VkInstance, 
 
     pub fn run(self: *HelloTriangleApplication) !void {
         self.initWindow();
@@ -20,9 +22,13 @@ pub const HelloTriangleApplication = struct {
        const width: c_int = comptime 800;
        const height: c_int = comptime 600;
        self.window = glfw.glfwCreateWindow(width, height, "Vulkan", null, null);
+       const error_code: c_int = glfw.glfwGetError(null);
+       if (error_code != 0) {
+           std.log.err("GLFW ERROR: {d}\n", .{error_code});
+       }
     } 
     fn initVulkan() void {
-        //TODO
+        createInstance();
     }
     fn mainLoop(self: *HelloTriangleApplication) void {
         assert(self.window != null);
@@ -33,5 +39,15 @@ pub const HelloTriangleApplication = struct {
     fn cleanup(self: *HelloTriangleApplication) void {
         glfw.glfwDestroyWindow(self.window);
         glfw.glfwTerminate();
+    }
+    fn createInstance() void {
+        const appinfo = vk.VkApplicationInfo {
+            .sType = vk.VK_STRUCTURE_TYPE_APPLICATION_INFO,
+            .pApplicationName = "Vulkan Triangle",
+            .applicationVersion = vk.VK_MAKE_VERSION(1, 0, 0),
+            .pEngineName = "No Engine",
+            .engineVersion = vk.VK_MAKE_VERSION(1, 0, 0),
+            .apiVersion = vk.VK_API_VERSION_1_0,
+        };
     }
 };
